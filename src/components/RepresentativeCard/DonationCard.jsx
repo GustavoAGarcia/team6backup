11e7e15a8b323b37179d93e63ce17fd645b699f9
gustavoagarcia <gustavoagarcia0808@gmail.com>
2019-12-03 20:39:41 -0600

    rileys stuff

--- src/components/RepresentativeCard/DonationCard.jsx
+++ src/components/RepresentativeCard/DonationCard.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
-var cashOnHand = 157958.64
-var  disbursements =  45990175.82;
-var receipts = 45668717.68;
+import axios from 'axios'
+import * as constants from '../../app/constants.jsx'
+import FECCaller from '../../app/FECCaller'
+
+var cashOnHand = '0';
+var disbursements = '0';
+var receipts = '0';
+
 //function used from https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
 function numberWithCommas(x) {
+    if (x === undefined) {
+        return 0
+    }
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
-export default class DonationCard extends Component
-{
-    constructor(props) { 
-        super(props) 
+
+export default class DonationCard extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            receipts:[],
+            committeeID:"NULL",
+            donationInfo: ""
+        }
         this.name = {
             name: props.name
         }
@@ -17,14 +31,32 @@ export default class DonationCard extends Component
             chamber: props.chamber
         }
     }
+
+    componentDidMount() {
+
+        let fecCaller = new FECCaller()
+         fecCaller.getMemberId(this.props.name)
+         .then(id => {
+             fecCaller.getFinanceInformation(id)
+             .then(results => {
+                 this.setState({
+                     donationInfo: results
+                 })
+                 console.log(results)
+             })
+         })
+    }
+
     render()
     {
-        return( 
-        <div className = 'listed-rep__container'> 
-        <h4> Campaign Finance </h4>
-        <li>Cash On Hand: ${numberWithCommas(cashOnHand)} </li>
-        <li>Total Receipts: ${numberWithCommas(disbursements)} </li>
-        <li>Total Disbursed: ${numberWithCommas(receipts)} </li>
+        return(
+        <div className = 'listed-rep__container'>
+            <h4>Campaign Finance </h4>
+            <li>Individual Contributions: ${numberWithCommas(this.state.donationInfo.individual_contributions)} </li>
+            <li>Campaign Contributions: ${numberWithCommas(this.state.donationInfo.contributions)} </li>
+            <li>Total loans: ${numberWithCommas(this.state.donationInfo.loans)} </li>
+            <li>Receipts: ${numberWithCommas(this.state.donationInfo.receipts)} </li>
+            <li>Disbursements: ${numberWithCommas(this.state.donationInfo.disbursements)} </li>
         </div>
         )
     }
